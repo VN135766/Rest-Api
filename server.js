f6da1ba83abe6e8114839a3db8f08496973bc392
Victor Nunez <VN135766@gmail.com>
2022-11-10 14:06:36 -0500

    added subscriber route

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to database'))
 
+app.use(express.json())
+
+const subscribersRouter = require('./routes/subscribers')
+app.use ('/subscribers', subscribersRouter)
 app.listen(3000, () => console.log('server is running'))
 
 
