676e9a786b2709fe188d5baa882cf91e7521372b
Victor Nunez <VN135766@gmail.com>
2022-11-15 15:41:44 -0500

    added rest starting points

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,4 +1,25 @@
 const express = require('express')
 const router = express.Router()
 
+//getting all subs
+router.get('/', (req, res) => {
+res.send('hello world')
+})
+// getting one sub
+router.post('/:id', (req, res) => {
+
+})
+// creating one sub
+router.post('/', (req, res) => {
+  res.send(req.params.id)
+})
+// updating one sub
+router.patch('/', (req, res) => {
+  res.send(req.params.id)
+})
+// deleting one
+router.delete('/:id', (req, res) => {
+  req.params.id
+})
+
 module.exports = router
