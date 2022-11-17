d78fa7c9b17fabea8564970b9a37ebb69575bd6c
Victor Nunez <VN135766@gmail.com>
2022-11-16 19:30:50 -0500

    cleaned up some code

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const Subscriber = require('../models/subscriber')
 
 //getting all subs
 router.get('/', (req, res) => {
 res.send('hello world')
 })
 // getting one sub
-router.post('/:id', (req, res) => {
-
+router.get('/:id', (req, res) => {
+  res.send(req.params.id)
 })
 // creating one sub
 router.post('/', (req, res) => {
